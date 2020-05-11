d601c3120ab3e82300ae67a1854073f546fefd5e
Jorge Lopez-Vargas <jalopez2@utpl.edu.ec>
2020-05-11 12:56:05 -0500

    Add taller3 folder and description

---
+++ taller3/code-taller3/src/main/java/ec/edu/utpl/cc/pavanzada/taller3/App.java
@@ -0,0 +1,13 @@
+package ec.edu.utpl.cc.pavanzada.taller3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
